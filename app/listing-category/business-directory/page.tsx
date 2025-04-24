28ba1ba3f08c850c533f9b8942f69cff0af24167
unknown <artut.123@mail.ru>
2025-04-24 07:01:02 +0400

    jnjel useParams@

--- app/listing-category/business-directory/page.tsx
+++ app/listing-category/business-directory/page.tsx
@@ -1,37 +1,15 @@
 "use client";
-import { useRouter } from "next/router";
-import { businessdirectoryItem } from "../../models/selectItems";
- import { useSearchParams } from 'next/navigation';
+
 export default function Business() {
-  const router = useSearchParams();
-  const id = router.get('id');
-  const selectedItems = businessdirectoryItem.filter(item => item.id === id)
   return (
     <div className="Home">
       <div className="flex">
       {/* Sidebar */}
       <aside className="w-64 bg-gray-100 p-4">
-        {businessdirectoryItem.map((item) => (
-          <div
-            key={item.id}
-            onClick={() => router.get(`/listing-category/${item.path}?id=${item.id}`)}
-            className={`cursor-pointer mb-2 ${
-              item.id === id ? 'font-bold text-purple-700' : ''
-            }`}
-          >
-            {item.label}
-          </div>
-        ))}
       </aside>
-
       {/* Main Content */}
       <main className="flex-1 p-4">
-        <h1 className="text-2xl font-semibold mb-4">{id}</h1>
-        {selectedItems.length > 0 ? (
-          <div>Here are the items for: <strong>{selectedItems[0].label}</strong></div>
-        ) : (
-          <div>Select a category from the left.</div>
-        )}
+        <h1 className="text-2xl font-semibold mb-4">h1</h1>
       </main>
     </div>
     </div>
--- app/listing-category/page.tsx
+++ app/listing-category/page.tsx
@@ -1,34 +1,18 @@
 'use client'
-import { useRouter } from 'next/router'
-import { businessdirectoryItem } from '@/app/models/selectItems' // change path
-import {  useMemo } from 'react'
-import { useParams, useSearchParams } from 'next/navigation'
-const Page = () => {
-  const params = useParams()
-  const path = params?.path // t
-  const searchParams = useSearchParams()
-  const id = searchParams.get('id')
-  const data = useMemo(() => {
-    if (!id || typeof id !== 'string') return []
 
-    if (path === 'business-directory') {
-      return businessdirectoryItem.filter((item) => item.id === id)
-    }
-    
-    return []
-  }, [path, id])
+
+const Page = () => {
+ 
   return (
     <div className="flex">
       <aside className="w-64 bg-gray-100 p-4">
-        <h2 className="font-bold mb-2">{path}</h2>
+        
         <ul>
-          {data.map((item) => (
-            <li key={item.id} className="py-1">{item.label}</li>
-          ))}
+          
         </ul>
       </aside>
       <main className="flex-1 p-4">
-        <h1>{id}page</h1>
+       <h1>page</h1>
       </main>
     </div>
   )
